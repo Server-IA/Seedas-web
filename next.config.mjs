a54b56734c90204a89ccfe0c827dddb45a860511
calossandres <163074147+calossandres@users.noreply.github.com>
2024-05-09 12:28:24 -0500

    module.exports=seguridad en ruta e imagenes c

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+/**  module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.example.com',
+        port: '',
+        pathname: '/account123/**',
+      },
+    ],
+  },        seguridad en la ruta de imagenes y acceso 
+} 
 
+*/
 export default nextConfig;
