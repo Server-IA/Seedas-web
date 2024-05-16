2c26b011ae2b21bb39dad9f47014f412e88b0894
calossandres <163074147+calossandres@users.noreply.github.com>
2024-05-15 22:18:03 -0500

    arreglo de solo mostrar indexpage con autentificacion

---
+++ app/about/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function about() {
+  return (
+  <>
+ 
+  <div>
+    <h2>paaginaa</h2>
+    </div>
+  
+  </>
+  )
+}
---
+++ app/indexPage/page.js
@@ -0,0 +1,17 @@
+// Importa los componentes necesarios de Clerk
+import { RedirectToHome, SignedIn } from "@clerk/nextjs";
+
+// Componente IndexPage
+export default function IndexPage() {
+  return (
+    <>
+      {/* Componente SignedIn para mostrar contenido solo a usuarios autenticados */}
+      <SignedIn>
+        {/* Contenido solo visible para usuarios autenticados */}
+        <div>Bienvenido a la página de inicio</div>
+      </SignedIn>
+      {/* Contenido adicional de la página */}
+      <div>inicie sesión para ver el contenido de la pagina ...</div>
+    </>
+  );
+}
